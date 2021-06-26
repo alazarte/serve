ede2a6897d49ce153d40c7c4de24ee733a458989
alazarte <inbox@alazarte.com>
2021-06-26 15:27:02 -0300

    Replace env with flags, remove allowHost

--- main.go
+++ main.go
@@ -13,65 +13,63 @@ import (
 )
 
 var (
-	skFilepath   string
-	pemFilepath  string
-	htmlFilepath string
-	publicPath   string
-	port         string
-	postURL      *url.URL
-	client       = http.Client{}
-	allowHost    = []string{"alazarte.com"}
+	errLogger  *log.Logger
+	infoLogger *log.Logger
+
+	postURL *url.URL
+
+	skFilepath   = flag.String("sk", "", "secret key filepath")
+	htmlFilepath = flag.String("html", "", "where html files are located")
+	pemFilepath  = flag.String("pem", "", "certificate filepath")
+	publicPath   = flag.String("public", "/tmp/public", "path to get public files")
+	port         = flag.String("port", "443", "port to listen for http connections")
+	urlString    = flag.String("post", "", "url to post to, to upload files")
+
+	client = http.Client{}
 )
 
 func init() {
-	var urlString string
-
-	flag.StringVar(&skFilepath, "sk", "", "secret key filepath")
-	flag.StringVar(&htmlFilepath, "html", "", "where html files are located")
-	flag.StringVar(&pemFilepath, "pem", "", "certificate filepath")
-	flag.StringVar(&publicPath, "public", "/tmp/public", "path to get public files")
-	flag.StringVar(&port, "port", "443", "port to listen for http connections")
-	flag.StringVar(&urlString, "post", "", "url to post to, to upload files")
 	flag.Parse()
 
-	if skFilepath == "" || pemFilepath == "" {
+	if *skFilepath == "" || *pemFilepath == "" {
 		fmt.Println("missing either pem or sk filepath")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	if htmlFilepath == "" {
+	if *htmlFilepath == "" {
 		fmt.Println("html source path is missing")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	if urlString == "" {
-		fmt.Println("missing url to post to")
-		flag.PrintDefaults()
-		os.Exit(1)
+	if *urlString != "" {
+		u, err := url.Parse(*urlString)
+		if err != nil {
+			fmt.Println("error parsing url:", err)
+			os.Exit(1)
+		}
+		postURL = u
 	}
-	u, err := url.Parse(urlString)
-	if err != nil {
-		fmt.Println("error parsing url:", err)
-		os.Exit(1)
-	}
-	postURL = u
-	if _, err := os.Stat(publicPath); err != nil {
-		if err := os.Mkdir(publicPath, 0755); err != nil {
+	if _, err := os.Stat(*publicPath); err != nil {
+		if err := os.Mkdir(*publicPath, 0755); err != nil {
 			fmt.Println(err)
 		}
 	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%s %s %s", r.Method, r.URL, r.RemoteAddr)
+	infoLogger.Printf("%s %s %s", r.Method, r.URL, r.RemoteAddr)
 
 	if r.Method != http.MethodPost && r.Method != http.MethodGet {
-		log.Println("invalid method:", r.Method)
+		errLogger.Println("invalid method:", r.Method)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	if r.Method == http.MethodPost {
+		if postURL == nil {
+			errLogger.Println("missing url to post to...")
+			return
+		}
 		r2 := new(http.Request)
 		*r2 = *r
 		r2.URL = postURL
@@ -79,19 +77,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 		res, err := client.Do(r2)
 		if err != nil {
-			fmt.Println(err)
+			errLogger.Println(err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		b, err := io.ReadAll(res.Body)
 		if err != nil {
-			fmt.Println("error reading response from API", err)
+			errLogger.Println("error reading response from API", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		w.WriteHeader(res.StatusCode)
 		if _, err := w.Write(b); err != nil {
-			log.Println(err)
+			errLogger.Println(err)
 		}
 		return
 	}
@@ -101,58 +99,48 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		r.URL.Path = "/index.html"
 	}
-	f, err := os.ReadFile("html" + r.URL.Path)
+	f, err := os.ReadFile(path.Join(*htmlFilepath, r.URL.Path))
 	if err != nil {
-		log.Println(err)
+		errLogger.Println(err)
 		w.WriteHeader(http.StatusNotFound)
 		f = []byte(http.StatusText(http.StatusNotFound))
 	}
 	if _, err := w.Write(f); err != nil {
-		log.Println(err)
+		errLogger.Println(err)
 	}
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
-	log.Println("redirecting information:")
-	fmt.Printf("%#v\n", r)
+	infoLogger.Printf("redirecting information: %#v\n", r)
 
 	if r.Method != http.MethodPost && r.Method != http.MethodGet {
-		log.Println("invalid method:", r.Method)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	found := false
-	for _, v := range allowHost {
-		if r.Host == v {
-			found = true
-			break
-		}
-	}
-	if !found {
-		log.Printf("%s not found in list of hosts: %v\n", r.Host, allowHost)
+		errLogger.Println("invalid method:", r.Method)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	target := "https://" + r.Host + r.URL.Path
-	// TODO handle query params?
 
-	log.Printf("redirecting to %s", target)
+	infoLogger.Printf("redirecting to %s", target)
 	http.Redirect(w, r, target, http.StatusTemporaryRedirect)
 }
 
+func init() {
+	errLogger = log.New(os.Stderr, "[error] ", log.LstdFlags)
+	infoLogger = log.New(os.Stdout, "[info] ", log.LstdFlags)
+}
+
 func main() {
-	log.Printf("listening on port 80 to redirect...")
+	infoLogger.Printf("listening on port 80 to redirect...")
 	go http.ListenAndServe(":80", http.HandlerFunc(redirect))
 
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/public/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s", r.Method, r.URL, r.RemoteAddr)
+		infoLogger.Printf("%s %s %s", r.Method, r.URL, r.RemoteAddr)
 		file := strings.TrimPrefix(r.URL.Path, "/public/")
 		http.ServeFile(w, r, fmt.Sprintf("%s/%s", publicPath, file))
 	})
 
-	log.Printf("going to serve on port 443, public path is %s", publicPath)
-	log.Fatal(http.ListenAndServeTLS(":443", pemFilepath, skFilepath, nil))
+	infoLogger.Printf("going to serve on port 443, public path is %s", publicPath)
+	errLogger.Fatal(http.ListenAndServeTLS(":443", *pemFilepath, *skFilepath, nil))
 }
